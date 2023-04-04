792f67b3770b9f98a07ee417cd25a5ef27d8ce54
Pujan Poudel <poudelpujan50@gmail.com>
2023-04-04 10:20:25 +0545

    add: fix error page issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.scss";
@@ -9,9 +9,9 @@ import MenuManager from "./components/Menu/MenuManager/MenuManager";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MenuManager>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </MenuManager>
   </React.StrictMode>
 );
